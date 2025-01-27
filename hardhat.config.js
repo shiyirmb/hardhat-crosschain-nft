bb1a11b6c43f187cc7048f75fe385937c6e066c7
gongyexiangjin <717054907@qq.com>
2025-01-27 12:26:45 +0800

    feat: 安装使用hardhat-deploy-ethers插件

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+// 使用hardhat-deploy-ethers插件编写合约部署脚本，
+// 安装及使用参考：https://github.com/wighawag/hardhat-deploy-ethers#readme
+require("@nomicfoundation/hardhat-ethers");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  namedAccounts: {
+    account1: {
+      default: 0,
+    },
+  },
 };
